da4cd2364d2c689b5886acc33872a7cad51bae48
Favour <ezehfavour1990@gmail.com>
2026-01-04 17:59:23 -0800

    designed the product page

---
+++ app/(root)/product/page.tsx
@@ -0,0 +1,8 @@
+import ProductsSection from '@/components/pages/products/products'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div><ProductsSection/></div>
+  )
+}
